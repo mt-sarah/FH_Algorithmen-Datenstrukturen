cea2eb342a355076c5ca863a56e3ac0f0db8294a
mt-sarah <berger-sarah@hotmail.com>
2022-03-13 21:32:56 +0100

    remove comments

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Exercise 1
-
 def name_age():
     name = input("Name: ")
     age = input("Age: ")
@@ -7,10 +5,6 @@ def name_age():
     result = name + age
     return result
 
-
-# print(name_age())
-
-# Exercise 2
 
 def swap_integers(num1, num2):
     x = num1
@@ -26,10 +20,6 @@ def swap_integers(num1, num2):
     return "{0}{1}".format(x, y)
 
 
-# print(swap_integers(10, 20))
-
-# Exercise 3
-
 def check_numbers(num1, num2):
     if (num1 and num2) % 10 == 0:
         if (num1 or num2) % 6 == 0:
@@ -40,11 +30,6 @@ def check_numbers(num1, num2):
     # alternative solution which is shorter but way harder to read:
     # return True if (num1 and num2) % 10 == 0 and (num1 or num2) % 6 == 0 else False
 
-
-# print(check_numbers(60, 30))
-
-
-# Exercise 4
 
 def sum_up(num1, num2):
     if 0 < num1 < num2:
@@ -57,10 +42,6 @@ def sum_up(num1, num2):
         return 0
 
 
-# print(sum_up(1, 3))
-
-# Exercise 5
-
 def circle_area(r1, r2, r3):
     def area(r):
         return 3.14 * r ** 2
@@ -68,19 +49,9 @@ def circle_area(r1, r2, r3):
     return area(r1) + area(r2) + area(r3)
 
 
-# print(circle_area(1, 2, 3))
-
-
-# Exercise 6
-
 def check_string(text):
     return text.casefold().startswith("a") or text.casefold().endswith("a")
 
-
-# print(check_string("jdklajljldjA"))
-
-
-# Exercise 7
 
 def triangle(rows):
     row = 1
@@ -91,5 +62,3 @@ def triangle(rows):
             star += 1
         print()
         row += 1
-
-# print(triangle(4))
