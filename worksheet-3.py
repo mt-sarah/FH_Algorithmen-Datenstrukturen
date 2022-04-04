847215ead22aec5f541740fc24f473f77ea3ef95
mt-sarah <berger-sarah@hotmail.com>
2022-04-04 18:51:59 +0200

    add exercise 5

--- worksheet-3.py
+++ worksheet-3.py
@@ -45,7 +45,6 @@ class Bus(Vehicle):
         return "Color: {0}, Fuel Type: {1}, Passengers: {2}".format(self.color, self.fuel_type, self.passengers)
 
 
-
 class Book:
     def __init__(self, name, author):
         self.name = name
@@ -56,7 +55,55 @@ class Book:
 
 
 class BookShelf:
-    def add_book_list(self, books):
-        self.books = books
+    def __init__(self):
+        self.books = []
+
+    def add_book_list(self, new_books):
+        for i in new_books:
+            if isinstance(i, Book):
+                self.books.append(i)
+
+    def books_by_author(self, author):
+        book_names = []
+        for i in self.books:
+            if i.author == author:
+                book_names.append(i.name)
+        return book_names
+
+    def get_books(self):
+        book_names = []
+        for i in self.books:
+            book_names.append(i.name)
+        return book_names
+
+    def clear_shelf(self):
+        self.books.clear()
 
 
+# book1 = Book("Harry Potter 1", "JK Rowling")
+# book2 = Book("Harry Potter 2", "JK Rowling")
+# book3 = Book("Harry Potter 3", "JK Rowling")
+# book4 = Book("Harry Potter 4", "JK Rowling")
+# book5 = Book("Lord of the Rings 1", "JRR Tolkien")
+# book6 = Book("Lord of the Rings 2", "JRR Tolkien")
+# book7 = Book("Lord of the Rings 3", "JRR Tolkien")
+# # book8 = Book("Good Omens")
+# book9 = ["American Gods", "Neil Gaiman"]
+# book10 = 42
+#
+
+# my_book_list = [book1, book2, book5, book7]
+#
+# my_shelf = BookShelf()
+#
+# my_shelf.add_book_list(my_book_list)
+
+# my_sorted_shelf = my_shelf.books_by_author("JK Rowling")
+# print(my_sorted_shelf)
+# print(my_shelf.books_by_author("JK Rowling"))
+# print(my_shelf.get_books())
+#
+# my_shelf.clear_shelf()
+# # my_sorted_shelf.clear_shelf()
+# print(my_sorted_shelf)
+# print(my_shelf.books_by_author("JK Rowling"))
